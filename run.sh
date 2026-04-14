892ebb846b31dbabe617d38744a061aaa44caaaa
Michael D. Catchen <mdcatchen@gmail.com>
2026-04-14 10:17:42 -0400

    fixes

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@
 #SBATCH --ntasks=1               
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=16G 
-#SBATCH --array=1-165 
+#SBATCH --array=123
 #SBATCH --time=3:00:00         
 
 export JULIA_DEPOT_PATH="/project/def-tpoisot/mcatchen/JuliaEnvironments/ColoradoBees"
 
 module load julia/1.11.3
-srun --unbuffered julia 04_fit_sdm.jl
+srun --unbuffered julia 05_fit_sdm.jl
