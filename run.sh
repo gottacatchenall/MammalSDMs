aa79afcc58bf3ad081056bc54ba60ea7bf4fd17c
Michael D. Catchen <mdcatchen@gmail.com>
2026-05-07 12:55:16 -0400

    array job

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1               
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=16G 
-#SBATCH --array=123
+#SBATCH --array=[1-166]
 #SBATCH --time=3:00:00         
 
 export JULIA_DEPOT_PATH="/project/def-tpoisot/mcatchen/JuliaEnvironments/ColoradoBees"
